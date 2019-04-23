a1833946dd4d4a43867b143246a6a34f8ec279d7
IlyaDobrynin <iliadobrynin@yandex.ru>
2019-04-23 23:25:00 +0300

    ref: add fpn_factory in init file

---
+++ ido_cv/src/models/__init__.py
@@ -0,0 +1,7 @@
+from .classification import classification_factory
+
+from .segmentation import unet_factory
+from .segmentation import deeplabv3
+from .segmentation import fpn_factory
+
+from .detection import retinanet
