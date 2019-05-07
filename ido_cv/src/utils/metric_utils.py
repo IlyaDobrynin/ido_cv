52f941a19126924d732c6689e1542f0e2deeee0f
IlyaDobrynin <iliadobrynin@yandex.ru>
2019-05-07 12:58:51 +0300

    feat: make metrics as classes

--- ido_cv/src/utils/metric_utils.py
+++ ido_cv/src/utils/metric_utils.py
@@ -1,10 +1,14 @@
 import numpy as np
 from tqdm import tqdm
 import cv2
+from sklearn.metrics import accuracy_score
 from .image_utils import resize_image
 
 
-def get_opt_threshold(trues, preds, threshold_min=0.1, metric_name='dice', device='cpu'):
+# SEGMENTATION
+
+
+def get_opt_threshold(trues, preds, threshold_min=0.1, metric_name='dice'):
     """ Function returns optimal threshold for prediction images
 
     :param trues: Ground truth validation masks
@@ -17,16 +21,9 @@ def get_opt_threshold(trues, preds, threshold_min=0.1, metric_name='dice', devic
     thresholds = np.linspace(0, 1, 50)
     preds_new = [np.uint8(preds > threshold) for threshold in tqdm(thresholds)]
 
-    if device == 'cpu':
-        metrics = np.array([numpy_metric(trues, pred, metric_name=metric_name)
-                            for pred in tqdm(preds_new)])
-    elif device == 'gpu':
-        metrics = np.array([torch_metric(trues, pred, metric_name=metric_name)
-                            for pred in tqdm(preds_new)])
-    else:
-        raise ValueError(
-            f'Wrong device parameter: {device}. Should be "cpu" or "gpu".'
-        )
+    metrics = np.array([numpy_metric_per_image(trues, pred, metric_name=metric_name)
+                        for pred in tqdm(preds_new)])
+
     threshold_best_index = np.argmax(metrics)
     best_metric = metrics[threshold_best_index]
     best_threshold = thresholds[threshold_best_index] \
@@ -34,7 +31,7 @@ def get_opt_threshold(trues, preds, threshold_min=0.1, metric_name='dice', devic
     return best_threshold, best_metric
 
 
-def torch_metric(trues, preds, metric_name):
+def torch_metric_per_image(trues, preds, metric_name):
     """ Function returns metrics calculated on GPU via pytorch
 
     :param preds: Predictions of the network
@@ -57,17 +54,17 @@ def torch_metric(trues, preds, metric_name):
         if true.long().sum() == 0 and pred.long().sum() == 0:
             continue
 
-        union = (true | pred).long().sum().float()
-        intersection = (true & pred).long().sum().float()
+        union = (true | pred).long().sum()
+        intersection = (true & pred).long().sum()
 
         if metric_name == 'dice':
-            metric = (2.0 * intersection + smooth) / (union + intersection + smooth)
+            metric = (2.0 * intersection + smooth) / (union + intersection + smooth).float()
             metric = metric.data.cpu().numpy()
         elif metric_name == 'jaccard':
-            metric = (intersection + smooth) / (union + smooth)
+            metric = (intersection + smooth) / (union + smooth).float()
             metric = metric.data.cpu().numpy()
         elif metric_name == 'm_iou':
-            iou = (intersection + smooth) / (union + smooth)
+            iou = (intersection + smooth) / (union + smooth).float()
             iou = iou.data.cpu().numpy()
             thresholds = np.arange(0.5, 1, 0.05)
             s = []
@@ -82,7 +79,41 @@ def torch_metric(trues, preds, metric_name):
     return np.mean(metrics)
 
 
-def numpy_metric(trues, preds, metric_name):
+def torch_metric(trues, preds, metric_name):
+    """ Function returns metrics calculated on GPU via pytorch
+
+    :param preds: Predictions of the network
+    :param trues: Ground truth labels
+    :param metric_name: Name of the metric to calculate
+    """
+    smooth = 1e-12
+    true = (trues == 1)
+    pred = (preds == 1)
+    union = (true | pred).long().sum()
+    intersection = (true & pred).long().sum()
+
+    if metric_name == 'dice':
+        metric = (2.0 * intersection + smooth) / (union + intersection + smooth).float()
+        metric = metric.data.cpu().numpy()
+    elif metric_name == 'jaccard':
+        metric = (intersection + smooth) / (union + smooth).float()
+        metric = metric.data.cpu().numpy()
+    elif metric_name == 'm_iou':
+        iou = (intersection + smooth) / (union + smooth).float()
+        iou = iou.data.cpu().numpy()
+        thresholds = np.arange(0.5, 1, 0.05)
+        s = []
+        for thresh in thresholds:
+            s.append(iou > thresh)
+        metric = np.mean(s)
+    else:
+        raise ValueError(
+            f'Wrong metric_name: {metric_name}. Should be "dice", "jaccard" or "m_iou".'
+        )
+    return metric
+
+
+def numpy_metric_per_image(trues, preds, metric_name):
     """ Function returns metric (dice, jaccard or mean IoU)
 
     :param y_true: True labels
@@ -130,8 +161,56 @@ def numpy_metric(trues, preds, metric_name):
     return np.mean(metrics)
 
 
+def numpy_metric(trues, preds, metric_name):
+    """ Function returns metric (dice, jaccard or mean IoU)
+
+    :param y_true: True labels
+    :param y_pred: Predicted labels
+    :param metric_name:
+    :param device: Device to calculate metric:
+                    - 'cpu'
+                    - 'gpu'
+    :return:
+    """
+    smooth = 1e-12
+    true = (trues == 1)
+    pred = (preds == 1)
+    union = np.sum(np.logical_or(true, pred).astype(np.uint8))
+    intersection = np.sum(np.logical_and(true, pred).astype(np.uint8))
+
+    # pred = resize_image(pred, size=true.shape[:2], interpolation=cv2.INTER_NEAREST)
+    # true_bool = np.asarray(true, dtype=bool)
+    # pred_bool = np.asarray(pred, dtype=bool)
+    # intersection = np.sum(np.logical_and(true, pred).astype(np.uint8))
+    # union = true_bool.sum() + pred_bool.sum()
+
+    if metric_name == 'dice':
+        metric = (2.0 * intersection + smooth) / (union + intersection + smooth)
+    elif metric_name == 'jaccard':
+        metric = (intersection + smooth) / (union + smooth)
+    elif metric_name == 'm_iou':
+        iou = (intersection + smooth) / (union + smooth)
+        thresholds = np.arange(0.5, 1, 0.05)
+        s = []
+        for thresh in thresholds:
+            s.append(iou > thresh)
+        metric = np.mean(s)
+    else:
+        raise ValueError(
+            f'Wrong metric_name: {metric_name}. Should be "dice", "jaccard" or "m_iou".'
+        )
+    return metric
+
+
 def calculate_confusion_matrix_from_arrays(prediction: np.ndarray, ground_truth: np.ndarray,
                                            nr_labels: int) -> np.ndarray:
+    """ Function calculate confusion matrix from the images arrays
+
+    :param prediction: Predicted images
+    :param ground_truth: True images
+    :param nr_labels: Number of classes on true image
+    :return:
+    """
     replace_indices = np.vstack((
         ground_truth.flatten(),
         prediction.flatten())
@@ -147,6 +226,16 @@ def calculate_confusion_matrix_from_arrays(prediction: np.ndarray, ground_truth:
 
 def get_metric_from_matrix(confusion_matrix: np.ndarray, metric_name: str,
                            ignore_class: int = None) -> list:
+    """ Function returns specified metric from confusion matrix
+
+    :param confusion_matrix: Confusion matrix of TP, TN, FP, FN for each class
+    :param metric_name: Name of metric to compute:
+                            - 'jaccard'
+                            - 'dice'
+
+    :param ignore_class: Class to ignore
+    :return:
+    """
     metrics = []
     if ignore_class is None:
         index_list = [i for i in range(confusion_matrix.shape[0])]
@@ -176,3 +265,15 @@ def get_metric_from_matrix(confusion_matrix: np.ndarray, metric_name: str,
                 iou = 2 * float(true_positives) / denom
         metrics.append(iou)
     return metrics
+
+
+# CLASSIFICATION
+
+
+def get_accuracy(trues, preds):
+    """ Function returns mean accuracy score for input batch of images
+
+    :return:
+    """
+    acc = accuracy_score(trues, preds)
+    return acc
